403b16e7ca28d611c2ea82307ee89ee9fe10a5a1
Erlbaum <leeba.erlbaum@accenture.com>
2020-03-02 15:41:01 -0600

    covered classes, objects, and object parameters)

---
+++ modules/books.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self, title, author, genre, tags):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.tags = tags
+
+# Example of a Class with an init method:
+# class Person:
+#     def __init__(self,name):
+#         self.name = name
+
+# Example of declaring an object:
+# Ellis = Person("Ellis")
+# Object must be declared after its class is defined
